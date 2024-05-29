9d82ec1e2804829aaff6e7857c87f0759e65225a
rasyidf <ufarasfa@gmail.com>
2024-05-29 23:00:27 +0700

    Update Package

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
-# React + Vite
+# Stradivary Common React Component Library
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+This is a common react component library that can be used in any react project. It is a collection of common components that are used in most of the projects.
 
-Currently, two official plugins are available:
+The Package is published on github and can be installed using npm. make sure to add the github token in the .npmrc file.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+## Installation
+
+```bash
+# npmrc file
+@stradivary:registry=https://npm.pkg.github.com
+//npm.pkg.github.com/:_authToken=YOUR_GITHUB_TOKEN
+```
+
+# Install the package
+```bash
+npm install @stradivary/common
+```
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { resolve } from 'node:path';
+import { extname, relative, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { glob } from 'glob';
 import nodePolyfills from 'rollup-plugin-polyfill-node';
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,19 +10,32 @@ export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: {
-      
-      entry: resolve(__dirname, 'lib/index.mjs'),
-      name: 'CommonLib',
-      // the proper extensions will be added
-      fileName: 'index',
+
+      entry: resolve(__dirname, 'lib/index.mjs'), 
+      formats: ['es'],
     },
     rollupOptions: {
       plugins: [nodePolyfills({
         buffer: true,
         crypto: true,
       })],
-      external: ['react', 'react-dom', 'react-router-dom', 'crypto', 'history'],
+      external: ['react', 'react/jsx-runtime', 'react-dom', 'react-router-dom', 'crypto', 'history'],
+      // input: Object.fromEntries(
+      //   glob.sync('lib/**/*.{js,jsx}', {
+      //     ignore: ["lib/**/*.d.ts", 'lib/__fixtures__/**'],
+      //   }).map(file => [
+      //     // The name of the entry point
+      //     relative(
+      //       'lib',
+      //       file.slice(0, file.length - extname(file).length)
+      //     ),
+      //     // The absolute path to the entry file
+      //     fileURLToPath(new URL(file, import.meta.url))
+      //   ])
+      // ),
       output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
